7c02eb4a4d835e67cf4090acd98c07c2ca8bac7b
jloiacano <j.loiacano55@gmail.com>
2020-04-14 12:03:12 -0500

    Add project files.

---
+++ NLECA Core Newsletter App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NLECA_Core_Newsletter_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
