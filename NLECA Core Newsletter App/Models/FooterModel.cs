38d35e3ab4e8a32fce0c17886d0d2490b05524cf
jloiacano <j.loiacano55@gmail.com>
2020-04-15 10:33:18 -0500

    added base for footer viewcomponent

---
+++ NLECA Core Newsletter App/Models/FooterModel.cs
@@ -0,0 +1,7 @@
+﻿namespace NLECA_Core_Newsletter_App.Models
+{
+    public class FooterModel
+    {
+        public string BrandName { get; set; }
+    }
+}
---
+++ NLECA Core Newsletter App/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLECA_Core_Newsletter_App.Models;
+
+namespace NLECA_Core_Newsletter_App.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            FooterModel footerModel = new FooterModel()
+            {
+                BrandName = "nleca footer"
+            };
+
+            return View(footerModel);
+        }
+    }
+}
