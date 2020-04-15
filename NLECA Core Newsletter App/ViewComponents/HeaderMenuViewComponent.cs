40f89bd7341041895c17e4a274a1e94c7ecc06ba
jloiacano <j.loiacano55@gmail.com>
2020-04-15 10:18:22 -0500

    added base for header menu viewcomponent

---
+++ NLECA Core Newsletter App/ViewComponents/HeaderMenuViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLECA_Core_Newsletter_App.Models;
+
+namespace NLECA_Core_Newsletter_App.ViewComponents
+{
+    public class HeaderMenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            HeaderMenuModel headerMenu = new HeaderMenuModel()
+            {
+                BrandName = "nleca new"
+            };
+
+            return View(headerMenu);
+        }
+    }
+}
