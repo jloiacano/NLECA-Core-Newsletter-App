dac21b73178753cfdf028c73fb59fc40b0cca67c
jloiacano <j.loiacano55@gmail.com>
2020-04-16 07:20:18 -0500

    added support for migrating to database in program.cs

--- NLECA Core Newsletter App/Program.cs
+++ NLECA Core Newsletter App/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLECA_Core_Newsletter_App.Data;
 
 namespace NLECA_Core_Newsletter_App
 {
@@ -13,7 +16,18 @@ namespace NLECA_Core_Newsletter_App
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = CreateHostBuilder(args).Build();
+            MigrateDatabase(host);
+            host.Run();
+        }
+
+        public static void MigrateDatabase(IHost host)
+        {
+            using (IServiceScope scope = host.Services.CreateScope())
+            {
+                ApplicationDbContext context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                context.Database.Migrate();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
