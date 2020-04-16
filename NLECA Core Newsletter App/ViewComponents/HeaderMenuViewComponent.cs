0d9a5544b076dae4f3daf903bbf4f6863ba750e5
jloiacano <j.loiacano55@gmail.com>
2020-04-16 11:37:51 -0500

    test for azure connection strings

--- NLECA Core Newsletter App/ViewComponents/HeaderMenuViewComponent.cs
+++ NLECA Core Newsletter App/ViewComponents/HeaderMenuViewComponent.cs
@@ -1,15 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using NLECA_Core_Newsletter_App.Models;
 
 namespace NLECA_Core_Newsletter_App.ViewComponents
 {
     public class HeaderMenuViewComponent : ViewComponent
     {
+        private readonly IConfiguration config;
+
+        public HeaderMenuViewComponent(IConfiguration config)
+        {
+            this.config = config;
+        }
         public IViewComponentResult Invoke()
         {
             HeaderMenuModel headerMenu = new HeaderMenuModel()
             {
-                BrandName = "nleca new"
+                BrandName = config.GetConnectionString("nlecaDBConnection")
             };
 
             return View(headerMenu);
