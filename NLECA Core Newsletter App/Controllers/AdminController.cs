03dea83482b90cb4a3e869e9491ce6b06716b326
jloiacano <j.loiacano55@gmail.com>
2020-04-27 06:28:20 -0500

    Updated header menu (more to come) and added a couple controllers groundwork

--- NLECA Core Newsletter App/Controllers/AdminController.cs
+++ NLECA Core Newsletter App/Controllers/AdminController.cs
@@ -1,20 +1,16 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace NLECA_Core_Newsletter_App.Controllers
 {
     public class AdminController : Controller
     {
-
-
         [Authorize(Roles = "SuperAdmin,Admin,ReadOnlyUser")]
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        [Authorize(Roles = "SuperAdmin,Admin,ReadOnlyUser")]
-        public IActionResult EditNewsletter()
         {
             return View();
         }
---
+++ NLECA Core Newsletter App/Controllers/NewsletterController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLECA_Core_Newsletter_App.Models.Newsletter;
+using NLECA_Core_Newsletter_App.Service.Interfaces;
+using System.Collections.Generic;
+
+namespace NLECA_Core_Newsletter_App.Controllers
+{
+    public class NewsletterController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
+        private readonly INewsletterService _newsletter;
+
+        public NewsletterController(ILogger<HomeController> logger, IConfiguration config, INewsletterService newsletter)
+        {
+            _logger = logger;
+            _config = config;
+            _newsletter = newsletter;
+        }
+
+        [Authorize(Roles = "SuperAdmin,Admin,ReadOnlyUser")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "SuperAdmin,Admin,ReadOnlyUser")]
+        public IActionResult NewsletterManager()
+        {
+            List<Newsletter> model = new List<Newsletter>();
+            return View(model);
+        }
+
+        [Authorize(Roles = "SuperAdmin,Admin,ReadOnlyUser")]
+        public IActionResult AlertManager()
+        {
+            List<Newsletter> model = new List<Newsletter>();
+            return View(model);
+        }
+    }
+}
