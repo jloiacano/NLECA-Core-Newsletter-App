7eba2770770e3dc71348b89b0776774507a95107
jloiacano <j.loiacano55@gmail.com>
2020-04-30 12:34:24 -0500

    Tweaks to fix/force build

--- Main NLECA Database/dbo/Tables/Articles.sql
+++ Main NLECA Database/dbo/Tables/Articles.sql
@@ -2,12 +2,12 @@
 (
 	[ArticleId] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[NewsletterId] INT NOT NULL,
-	[Sequence] INT NOT NULL,
+	[Sequence] INT NOT NULL DEFAULT 0,
 	[ImageFileLocation] VARCHAR(256) NULL,
 	[ArticleType] INT NOT NULL DEFAULT 0,
-	[Text] VARCHAR(MAX) NOT NULL, 
-	[AddedBy] INT NOT NULL,
-	[DateAdded] DATETIME NOT NULL,
+	[Text] VARCHAR(MAX) NOT NULL DEFAULT 'Must be updated', 
+	[AddedBy] INT NOT NULL DEFAULT 0,
+	[DateAdded] DATETIME NOT NULL DEFAULT 0,
     CONSTRAINT Article_Newsletter
 		FOREIGN KEY (NewsletterId)
 		REFERENCES Newsletters (NewsletterId)
