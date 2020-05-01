39569be5ed43bbecc78c6e9847b5fa2db211b502
jloiacano <j.loiacano55@gmail.com>
2020-04-30 20:48:38 -0500

    Imported database

---
+++ Database/Security/nlecaApp_1.sql
@@ -0,0 +1,3 @@
+﻿CREATE LOGIN [nlecaApp]
+    WITH PASSWORD = N'jbauvBOnZhqiicitjqrhzm8nmsFT7_&#$!~<dr$4nEd`$cJv';
+
---
+++ Database/dbo/Tables/Newsletters.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Newsletters] (
+    [NewsletterId] INT IDENTITY(1,1) NOT NULL,
+    [CreatedDate] DATETIME,
+    [CreatedBy] INT,
+    [Memo] VARCHAR(256) NULL,
+    [DisplayDate] VARCHAR(256) NULL ,
+    [PublishedDate] DATETIME,
+    [IsCurrent] BIT NOT NULL DEFAULT 0,
+
+    PRIMARY KEY CLUSTERED ([NewsletterId] ASC) 
+);
