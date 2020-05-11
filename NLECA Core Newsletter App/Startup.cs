b4ac92bef06d2bd696dc28470aabcb556f557357
jloiacano <j.loiacano55@gmail.com>
2020-05-11 11:14:54 -0500

    temp using developer exception page for all environments

--- NLECA Core Newsletter App/Startup.cs
+++ NLECA Core Newsletter App/Startup.cs
@@ -80,7 +80,8 @@ namespace NLECA_Core_Newsletter_App
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseDeveloperExceptionPage();
+                //app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
