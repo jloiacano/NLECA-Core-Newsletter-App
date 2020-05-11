9d3c982bf3a83f3611b0dba9039b89a6433bb2c3
jloiacano <j.loiacano55@gmail.com>
2020-05-11 13:52:31 -0500

    switched back exception page

--- NLECA Core Newsletter App/Startup.cs
+++ NLECA Core Newsletter App/Startup.cs
@@ -80,8 +80,7 @@ namespace NLECA_Core_Newsletter_App
             }
             else
             {
-                app.UseDeveloperExceptionPage();
-                //app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
