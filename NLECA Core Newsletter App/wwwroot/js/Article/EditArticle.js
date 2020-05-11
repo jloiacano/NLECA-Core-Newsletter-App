226d8d72959c0043bcf05a6a63cd14974e0ee2b4
jloiacano <j.loiacano55@gmail.com>
2020-05-11 14:24:18 -0500

    Added back in all the stuff for ckeditor5

---
+++ NLECA Core Newsletter App/wwwroot/js/Article/EditArticle.js
@@ -0,0 +1,19 @@
+﻿var EditArticle = EditArticle || {};
+
+$(document).ready(function () {
+    EditArticle.init();
+});
+
+EditArticle = {
+
+    init: function () {
+        var articleEditor = ClassicEditor
+            .create(document.querySelector('#articleEditor'))
+            .then(editor => {
+                console.log(editor);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+}
