6d1ef6c4d33fbccaa319509f265415eb85500ff6
jloiacano <j.loiacano55@gmail.com>
2020-05-16 21:54:57 -0500

    Added Scaffolding for Identity

---
+++ NLECA Core Newsletter App/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NLECA_Core_Newsletter_App.Data;
+
+[assembly: HostingStartup(typeof(NLECA_Core_Newsletter_App.Areas.Identity.IdentityHostingStartup))]
+namespace NLECA_Core_Newsletter_App.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
