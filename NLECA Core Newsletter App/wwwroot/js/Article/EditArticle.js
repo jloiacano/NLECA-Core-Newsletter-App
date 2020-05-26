3254a7d4435d3f11e7524d67a78bc7e9ba7b427b
jloiacano <j.loiacano55@gmail.com>
2020-05-26 09:42:06 -0500

    top bottom left right images viewable

--- NLECA Core Newsletter App/wwwroot/js/Article/EditArticle.js
+++ NLECA Core Newsletter App/wwwroot/js/Article/EditArticle.js
@@ -7,7 +7,14 @@ $(document).ready(function () {
 EditArticle = {
 
     init: function () {
-        CKEDITOR.replace('articleEditor');
+        CKEDITOR.replace('ArticleText');
+
+        EditArticle.ShowCorrectArticleImageLocation($('#ArticleTypeDropdown').val());
+
+        $('#ArticleTypeDropdown').change(function () {
+            console.log($('#ArticleTypeDropdown option:selected').text());
+            EditArticle.ShowCorrectArticleImageLocation($(this).val());
+        });
 
         $('#ArticleEditCancelButton').click(function () {
             // TODO - J - go back
@@ -16,6 +23,42 @@ EditArticle = {
         $('#ArticleDeleteButton').click(function () {
             EditArticle.DeleteArticle();
         });
+    },
+
+    HideAllImages: function () {
+        $('.leftArticleImage').hide();
+        $('.rightArticleImage').hide();
+        $('.topArticleImage').hide();
+        $('.bottomArticleImage').hide();
+
+    },
+
+    ShowCorrectArticleImageLocation: function (articleTypeInt) {
+        switch (articleTypeInt) {
+            case '1':
+                //show left
+                EditArticle.HideAllImages();
+                $('.leftArticleImage').show()
+                break;
+            case '2':
+                //show right
+                EditArticle.HideAllImages();
+                $('.rightArticleImage').show()
+                break;
+            case '3':
+                //show top
+                EditArticle.HideAllImages();
+                $('.topArticleImage').show()
+                break;
+            case '4':
+                //show bottom
+                EditArticle.HideAllImages();
+                $('.bottomArticleImage').show()
+                break;
+            default:
+                //do no image;
+                EditArticle.HideAllImages();
+        }
     },
 
     DeleteArticle: function () {
