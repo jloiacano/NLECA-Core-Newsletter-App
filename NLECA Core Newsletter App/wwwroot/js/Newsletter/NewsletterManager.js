6b2496840d78d0d55caecc5cd2f82c0a5ec8eccb
jloiacano <j.loiacano55@gmail.com>
2020-06-05 15:34:45 -0500

    added dialogs to NewsletterManager

--- NLECA Core Newsletter App/wwwroot/js/Newsletter/NewsletterManager.js
+++ NLECA Core Newsletter App/wwwroot/js/Newsletter/NewsletterManager.js
@@ -5,7 +5,50 @@ $(document).ready(function () {
 });
 
 NewsletterManager = {
+
     init: function () {
+
+        // DIALOGS
+        $('#UnpublishNewsletterDialog').dialog({
+            autoOpen: false
+            , modal: true
+            , hide: "slide"
+            , show: "slide"
+            , buttons: {
+                YES: function () {
+                    NewsletterManager.UnpublishNewsletter($(this).data('newsletterId'));
+                }
+                ,NO: function () { $(this).dialog("close"); }
+            },
+        });
+
+        $('#PublishNewsletterDialog').dialog({
+            autoOpen: false
+            , modal: true
+            , hide: "slide"
+            , show: "slide"
+            , buttons: {
+                YES: function () {
+                    NewsletterManager.PublishNewsletter($(this).data('newsletterId'));
+                }
+                , NO: function () { $(this).dialog("close"); }
+            },
+        });
+
+        $('#DeleteNewsletterDialog').dialog({
+            autoOpen: false
+            , modal: true
+            , hide: "slide"
+            , show: "slide"
+            , buttons: {
+                "YES, DELETE IT": function () {
+                    NewsletterManager.DeleteNewsletter($(this).data('newsletterId'));
+                }
+                , "NO": function () { $(this).dialog("close"); }
+            },
+        });
+
+        // BUTTONS
         $('#AddNewsletterButton').click(function () {
             window.location.href = '/Newsletter/AddNewsletter/';
         });
@@ -16,18 +59,33 @@ NewsletterManager = {
         });
 
         $('#DeleteNewsletterButton').click(function (e) {
-            var newsletterId = $(e.target).closest('.individualNewletter').data('newsletterid');
-            window.location.href = '/Newsletter/RemoveNewsletter/?newsletterId=' + newsletterId;
+            $("#DeleteNewsletterDialog")
+                .data('newsletterId', $(e.target).closest('.individualNewletter').data('newsletterid'))
+                .dialog('open');
         });
 
         $('#PublishNewsletterButton').click(function (e) {
-            var newsletterId = $(e.target).closest('.individualNewletter').data('newsletterid');
-            window.location.href = '/Newsletter/PublishNewsletter/?newsletterId=' + newsletterId;
+            $("#PublishNewsletterDialog")
+                .data('newsletterId', $(e.target).closest('.individualNewletter').data('newsletterid'))
+                .dialog('open');
         });
 
         $('#UnpublishNewsletterButton').click(function (e) {
-            var newsletterId = $(e.target).closest('.individualNewletter').data('newsletterid');
-            window.location.href = '/Newsletter/UnpublishNewsletter/?newsletterId=' + newsletterId;
+            $("#UnpublishNewsletterDialog")
+                .data('newsletterId', $(e.target).closest('.individualNewletter').data('newsletterid'))
+                .dialog('open');
         });
     },
+
+    PublishNewsletter: function (newsletterId) {
+        window.location.href = '/Newsletter/PublishNewsletter/?newsletterId=' + newsletterId;
+    },
+
+    UnpublishNewsletter: function (newsletterId) {
+        window.location.href = '/Newsletter/UnpublishNewsletter/?newsletterId=' + newsletterId;
+    },
+
+    DeleteNewsletter: function (newsletterId) {
+        window.location.href = '/Newsletter/RemoveNewsletter/?newsletterId=' + newsletterId;
+    }
 }
