6800f65c4b6e2f92a1d7c828f3cb11f43f1791ec
jloiacano <j.loiacano55@gmail.com>
2020-06-08 00:04:03 -0500

    added to IAlertService, documented, and implemented (notimplemented) in AlertService

--- NLECA Core Newsletter App/Service/Interfaces/IAlertService.cs
+++ NLECA Core Newsletter App/Service/Interfaces/IAlertService.cs
@@ -1,11 +1,70 @@
-﻿using System;
+﻿using NLECA_Core_Newsletter_App.Models.Alert;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NLECA_Core_Newsletter_App.Service.Interfaces
 {
     public interface IAlertService
     {
+        /// <summary>
+        /// Adds an alert to the database
+        /// </summary>
+        /// <param name="alertModel">AlertModel to be added</param>
+        /// <returns>true if AlertModel was successfully added to database</returns>
+        bool AddAlert(AlertModel alertModel);
+
+        /// <summary>
+        /// Gets a single alert, and its details, from the database
+        /// </summary>
+        /// <param name="AlertId">The Id of the Alert</param>
+        /// <returns>A single AlertModel with all the alert details</returns>
+        AlertModel GetAlertById(int AlertId);
+
+        /// <summary>
+        /// Gets all of the Alerts in the database
+        /// </summary>
+        /// <returns>All of the Alerts in the database</returns>
+        IEnumerable<AlertModel> GetAllAlerts();
+
+        /// <summary>
+        /// Gets all of the Alerts in the database which have been published
+        /// </summary>
+        /// <returns>All of the Alerts in the database which have been published</returns>
+        IEnumerable<AlertModel> GetAllPublishedAlerts();
+
+        /// <summary>
+        /// Gets all of the Alerts in the database occuring after now
+        /// </summary>
+        /// <returns>All of the Alerts in the database occuring after now</returns>
+        IEnumerable<AlertModel> GetAllFutureAlerts();
+
+        /// <summary>
+        /// Updates an existing AlertModel in the database
+        /// </summary>
+        /// <param name="alertModel">AlertModel with the updates</param>
+        /// <returns>true if AlertModel was successfully updated</returns>
+        bool UpdateAlert(AlertModel alertModel);
+
+        /// <summary>
+        /// Publishes the Alert by its AlertId
+        /// </summary>
+        /// <param name="alertId">Id of the Alert to publish</param>
+        /// <returns>true if the alert was successfully published</returns>
+        bool PublishAlert(int alertId);
+
+        /// <summary>
+        /// Unpublishes the Alert by its AlertId
+        /// </summary>
+        /// <param name="alertId">Id of the Alert to unpublish</param>
+        /// <returns>true if the alert was successfully unpublishted</returns>
+        bool UnpublishAlert(int alertId);
+
+        /// <summary>
+        /// Deletes the Alert by its AlertId
+        /// </summary>
+        /// <param name="alertId">Id of the Alert to delete</param>
+        /// <returns>true if the alert was successfully deleted</returns>
+        bool DeleteAlert(int alertId);
+
     }
 }
--- NLECA Core Newsletter App/Service/Services/AlertService.cs
+++ NLECA Core Newsletter App/Service/Services/AlertService.cs
@@ -1,4 +1,6 @@
-﻿using NLECA_Core_Newsletter_App.Service.Interfaces;
+﻿using Microsoft.Extensions.Logging;
+using NLECA_Core_Newsletter_App.Models.Alert;
+using NLECA_Core_Newsletter_App.Service.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +10,58 @@ namespace NLECA_Core_Newsletter_App.Service.Services
 {
     public class AlertService : IAlertService
     {
+        private readonly ILogger<AlertService> _logger;
+        private readonly ISQLHelperService _sql;
+
+        public AlertService(ILogger<AlertService> logger, ISQLHelperService sql)
+        {
+            _logger = logger;
+            _sql = sql;
+        }
+
+        public bool AddAlert(AlertModel alertModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteAlert(int alertId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AlertModel GetAlertById(int AlertId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<AlertModel> GetAllAlerts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<AlertModel> GetAllFutureAlerts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<AlertModel> GetAllPublishedAlerts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PublishAlert(int alertId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UnpublishAlert(int alertId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateAlert(AlertModel alertModel)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
